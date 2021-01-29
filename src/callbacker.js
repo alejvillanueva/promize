bc4e6c55614f74ee901f60078b6a0fd99f9c91e3
Alejandra Villanueva <alejyvilla@gmail.com>
2021-01-28 23:36:42 -0500

    Finished syncCallBacker

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,5 +1,23 @@
-const syncCallbacker = null;
+const syncCallbacker = function (funcOne, funcTwo, ...funcs) {
+  if (typeof funcOne !== 'function' || typeof funcTwo !== 'function') {
+    throw 'You need to feed me functions';
+  }
 
-const asyncCallbacker = null;
+  let results = funcTwo(funcOne());
+
+  if (funcs.length) {
+    results = funcs.reduce((acc, currFunc) => {
+      return currFunc(acc);
+    }, results);
+  }
+
+  return results;
+};
+
+const asyncCallbacker = function (funcOne, funcTwo, ...funcs) {
+  if (typeof funcOne !== 'function' || typeof funcTwo !== 'function') {
+    throw 'You need to feed me functions';
+  }
+};
 
 module.exports = { syncCallbacker, asyncCallbacker };
