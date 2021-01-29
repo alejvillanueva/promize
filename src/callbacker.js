0f626c73b97d3364ffefb9264ba73fac0f89f583
Alejandra Villanueva <alejyvilla@gmail.com>
2021-01-28 23:43:35 -0500

    Added a check function

--- src/callbacker.js
+++ src/callbacker.js
@@ -6,6 +6,11 @@ const syncCallbacker = function (funcOne, funcTwo, ...funcs) {
   let results = funcTwo(funcOne());
 
   if (funcs.length) {
+    const areAllFuncs = funcs.every((func) => typeof func === 'function');
+    if (!areAllFuncs) {
+      throw 'Something is a NOT a function';
+    }
+
     results = funcs.reduce((acc, currFunc) => {
       return currFunc(acc);
     }, results);
@@ -18,6 +23,13 @@ const asyncCallbacker = function (funcOne, funcTwo, ...funcs) {
   if (typeof funcOne !== 'function' || typeof funcTwo !== 'function') {
     throw 'You need to feed me functions';
   }
+
+  /*
+  let areAllFuncs = funcs.every((func) => typeof func === 'function');
+    if (!areAllFuncs) {
+      throw 'Something is a function';
+    }
+  */
 };
 
 module.exports = { syncCallbacker, asyncCallbacker };
